714ff9782326bebbe5289fda817c38c3db2e4a11
yvonne0414 <yvonne9456@gmail.com>
2022-06-08 18:52:07 +0800

    style import antd bootstrap.scss
    引入antd、bootstrap、全域.scss

--- injoin-react/src/index.js
+++ injoin-react/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.dark.css';
+import './assets/scss/index.scss';
+// css
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

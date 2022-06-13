d4edaeaae5587c28af54c91c9b06d66faf54880a
yvonne0414 <yvonne9456@gmail.com>
2022-06-13 21:59:11 +0800

    chore 移除不需要的部分

--- injoin-react/src/components/FePage1Header/index.js
+++ injoin-react/src/components/FePage1Header/index.js
@@ -5,7 +5,7 @@ const FePage1Header = (props) => {
   let titleEnCaption = titleEn.substring(0, 1).toUpperCase();
   let titleEnContent = titleEn.substring(1).toLowerCase();
   let selectOptions = pageSelector.selectOptions;
-  console.log(selectOptions);
+
   return (
     <div className="page-type1-header">
       {pageSelector.isShow && (
@@ -35,22 +35,12 @@ const FePage1Header = (props) => {
             {menuList.map((menu, i) => {
               return (
                 <li key={i} className={i === 0 ? 'active' : ''}>
-                  <a href={menu.herf}>{menu.name}</a>
+                  <a href={menu.href} value={i}>
+                    {menu.name}
+                  </a>
                 </li>
               );
             })}
-            {/* <li>
-              <a href="/">INJION 主辦活動</a>
-            </li>
-            <li className="active">
-              <a href="/">私人開團活動</a>
-            </li>
-            <li>
-              <a href="/">我要開團</a>
-            </li>
-            <li>
-              <a href="/">報名參加</a>
-            </li> */}
           </ul>
         </div>
       </div>
@@ -58,7 +48,6 @@ const FePage1Header = (props) => {
         <img src={`images/fe/page1header/${imgs.m}`} className="img-fluid object-cover m-view" alt={imgs.m} />
         <img src={`images/fe/page1header/${imgs.pc}`} className="img-fluid object-cover pc-view" alt={imgs.pc} />
         <div className="page-type1-header-img-mask"></div>
-        {/* <!-- 記得加結尾標籤（沒有加react會壞掉 --> */}
       </div>
     </div>
   );

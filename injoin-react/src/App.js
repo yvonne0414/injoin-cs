2bcb45ab22bbb8cf918371317b39d2f7032e788f
yvonne0414 <yvonne9456@gmail.com>
2022-06-13 18:53:00 +0800

    chore 設定router config

--- injoin-react/src/App.js
+++ injoin-react/src/App.js
@@ -1,12 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FeHeader from './components/FeHeader';
+import GroupList from './views/frontEnd/GroupList/index';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <FeHeader />
-    </>
+      <main>
+        <Routes>
+          <Route path="/group" exact element={<GroupList />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
--- injoin-react/src/utils/config.js
+++ injoin-react/src/utils/config.js
@@ -1,2 +1,3 @@
 const BASE_URL = process.env.REACT_APP_SS_URL || 'http://localhost:3001';
 export const API_URL = BASE_URL + '/api';
+export const IMAGE_URL = BASE_URL + '/images';

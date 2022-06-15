e1b2d12ca0a29bac9b5ac1322f2ce79233dd15a5
Ariel-mcu <judy3113461@gmail.com>
2022-06-15 20:06:11 +0800

    fix productionDetail

--- injoin-react/src/views/frontEnd/ProductionDetail/index.js
+++ injoin-react/src/views/frontEnd/ProductionDetail/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
-import { Breadcrumb, Carousel } from 'antd';
+import { Breadcrumb, Carousel, Rate } from 'antd';
 
 import prddetailImg1 from '../../../assets/images/fe/productionDetail/prd-detail-img-1.png';
 import prddetailImg2 from '../../../assets/images/fe/productionDetail/prd-detail-img-2.png';
@@ -38,30 +38,31 @@ const ProductionDetail = () => {
             </div>
           </div>
           <div>
-          <div style={contentStyle}>
+            <div style={contentStyle}>
               <img src={prddetailImg2} alt="prd-detail-img-2" className="mx-auto h-100" />
             </div>
           </div>
           <div>
-          <div style={contentStyle}>
+            <div style={contentStyle}>
               <img src={prddetailImg3} alt="prd-detail-img-3" className="mx-auto h-100" />
             </div>
           </div>
           <div>
-          <div style={contentStyle}>
+            <div style={contentStyle}>
               <img src={prddetailImg4} alt="prd-detail-img-4" className="mx-auto h-100" />
             </div>
           </div>
         </Carousel>
         <div className="container">
           <div className="pc-view">
-          <div className="prd-detail-img-1">
-            <img src={prddetailImg1} alt="prd-detail-img-1"/>
+            <div className="prd-detail-img-1">
+              <img src={prddetailImg1} alt="prd-detail-img-1" />
+            </div>
           </div>
-          </div>
-          <div className="prd-detail-title">
-          金賓黑波爾本威士忌
-          </div>
+          <div className="prd-detail-title mt-4">金賓黑波爾本威士忌</div>
+          <div className="prd-detial-price mt-3">NT. 550</div>
+          <div className="star-defaultValue mt-3"><Rate disabled defaultValue={5} /></div>
+          <div className="prd-detail-number mt-3">數量</div>
         </div>
       </div>
     </>

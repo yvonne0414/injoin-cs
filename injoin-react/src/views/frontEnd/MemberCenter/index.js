cac56025b63657c859332a18cc6b2ee51bfd0127
Cheer <luap580101@gmail.com>
2022-06-15 15:07:05 +0800

    feat membercenter

--- injoin-react/src/views/frontEnd/MemberCenter/index.js
+++ injoin-react/src/views/frontEnd/MemberCenter/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import FePage1Header from '../../../components/FePage1Header';
 import './-membercenter.scss';
+import Button from 'react-bootstrap/Button';
+import iconURL from '../../../assets/images/fe/membercenter/usericon.png'
+
 
 const MemberCenter = () => {
+  
   const page1HeaderInfo = {
     titleEn: 'Member',
     titleCn: '會員中心',
@@ -49,12 +53,20 @@ const MemberCenter = () => {
           <div className="page-type1-area-title">會員資料</div>
           <div className="member-box">
             <nav className="memberbox-nav">
-              <ul className='memberbox-ul list-unstyled'>
-                <li>個人檔案</li>
-                <li>關於我</li>
-                <li>更改密碼</li>
+              <ul className="memberbox-ul list-unstyled d-flex row">
+                <li className="col-4 active">個人檔案</li>
+                <li className="col-4">關於我</li>
+                <li className="col-4">更改密碼</li>
               </ul>
             </nav>
+            <div className="member-card">
+              <div className="member-user">
+                <div className="user-img">
+                  <img src={iconURL} alt="" />
+                </div>
+                <Button className='user-button' variant="light">編輯照片</Button>
+              </div>
+            </div>
           </div>
         </section>
       </div>

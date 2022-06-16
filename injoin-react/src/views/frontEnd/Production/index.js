753c19289e434754a65dd4d31e039599743ee34d
shu10111 <pkathy111@gmail.com>
2022-06-16 10:48:01 +0800

    style production select(篩選)

--- injoin-react/src/views/frontEnd/Production/index.js
+++ injoin-react/src/views/frontEnd/Production/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.scss';
-import { FaSortAmountDownAlt } from 'react-icons/fa';
-import { FaChevronRight } from 'react-icons/fa';
+// import { FaSortAmountDownAlt } from 'react-icons/fa';
+// import { FaChevronRight } from 'react-icons/fa';
 // import { Form, Input } from 'antd';
 
 import FePage2Header from '../../../components/FePage2Header';
@@ -46,12 +46,38 @@ const Production = () => {
       <FePage2Header isProduct={isProduct} sectionBg={sectionBg} subTitle={subTitle} majorTitle={majorTitle} prdImg={prdImg} navs={navs} />
       <div className="prd-content">
         <div className="container">
-          <div className="prd-total">
-            <span>副材料</span>
-            <span>/</span>
-            <span>共16件商品</span>
+          <div className="prd-content-top">
+            <div className="prd-total">
+              <span>副材料</span>
+              <span>/</span>
+              <span>共16件商品</span>
+            </div>
+            <div className="prd-sel-all">
+              <div className="prd-sel-1">
+                <select value="" className=" mx-2 px-2 prd-sel">
+                  {/* <optgroup labal="基酒種類"></optgroup> */}
+                  <option value="" className="prd-sel-option">
+                    威士忌
+                  </option>
+                </select>
+                <select value="" className="mx-2 px-2 prd-sel">
+                  <option value="" className="prd-sel-option">
+                    蘇格蘭威士忌
+                  </option>
+                </select>
+              </div>
+              <div className="prd-sel-2 d-flex align-items-end ms-4 mt-1">
+                <span>依</span>
+                <select value="" className="mx-2 px-2 prd-sel">
+                  <option value="" className="prd-sel-option">
+                    價格高到低
+                  </option>
+                </select>
+                <span>排序</span>
+              </div>
+            </div>
           </div>
-          <div className="prd-sel">
+          {/* <div className="prd-sel">
             <div className="prd-sel-phone">
               <button className="prd-sel-btn">
                 <FaSortAmountDownAlt className="prd-sel-btn-icondown" />
@@ -64,7 +90,7 @@ const Production = () => {
                 <FaChevronRight className="prd-sel-btn-iconright" />
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
         <div>
           {/* <div className="prd-sel-pc">
@@ -84,27 +110,10 @@ const Production = () => {
           </div> */}
           <form className="prd-search-form d-flex">
             <input className="prd-search-label form-control form-control-sm me-2" type="search" placeholder="Search" />
-            <button className="btn prd-sel-pc-btn" type="submit">
+            <button className="btn prd-search-btn" type="submit">
               搜尋
             </button>
           </form>
-          <div className="prd-sel-pc">
-            <div>
-              <select value="produtlist" className="prd-sel-pcname mx-2 px-2">
-                <option value="">威士忌</option>
-              </select>
-              <select value="produtlist" className="prd-sel-pcname mx-2 px-2">
-                <option value="">蘇格蘭威士忌</option>
-              </select>
-            </div>
-            <div>
-              <span>依</span>
-              <select value="produtlist" className="prd-sel-pcname mx-2 px-2">
-                <option value="">價格高到低</option>
-              </select>
-              <span>排序</span>
-            </div>
-          </div>
         </div>
       </div>
     </>

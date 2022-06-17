bdaf232b0663a76d63f3a5c2b852b79925b2f756
Cheer <luap580101@gmail.com>
2022-06-17 13:58:58 +0800

    feat uservippp

--- injoin-react/src/views/frontEnd/UserVip/index.js
+++ injoin-react/src/views/frontEnd/UserVip/index.js
@@ -3,6 +3,7 @@ import FePage1Header from '../../../components/FePage1Header';
 import vipimg from '../../../assets/images/fe/uservip/vips2pic.png';
 import vips3img from '../../../assets/images/fe/uservip/vips3pic.png';
 import vips5img from '../../../assets/images/fe/uservip/vips5pic.png';
+import vips4img from '../../../assets/images/fe/uservip/gold.png';
 
 const page1HeaderInfo = {
   titleEn: 'VIP',
@@ -46,8 +47,8 @@ const UserVip = () => {
   return (
     <>
       <div className="container">
-        {/* <FePage1Header titleEn={titleEn} titleCn={titleCn} menuList={menuList} imgs={imgs} pageSelector={pageSelector} /> */}
-        <section className="uservip-s1 d-none">
+        <FePage1Header titleEn={titleEn} titleCn={titleCn} menuList={menuList} imgs={imgs} pageSelector={pageSelector} />
+        <section className="uservip-s1">
           <div className="page-type1-area-title">如何成為VIP會員</div>
           <div className="uservips1-title">需先至官網註冊會員才能累計消費</div>
           <ul className="uservips1-ul">
@@ -57,7 +58,7 @@ const UserVip = () => {
           </ul>
           <div className="uservips1-notice">*消費累積,一律依照會員註冊Email累計, 忘記登入或不同人購買, 皆無法合併計算累積*</div>
         </section>
-        <section className="uservip-s2 d-none">
+        <section className="uservip-s2">
           <div className="uservips2-title">會員分級標準</div>
           <div className="uservips2-content">
             <div className="vip-part">
@@ -74,7 +75,7 @@ const UserVip = () => {
             <img src={vipimg} alt="" />
           </div>
         </section>
-        <seciton className="uservip-s3 d-none">
+        <seciton className="uservip-s3">
           <div className="uservips3-img">
             <img src={vips3img} alt="" />
           </div>
@@ -90,7 +91,58 @@ const UserVip = () => {
             </ul>
           </div>
         </seciton>
-        <section className="uservip-s4"></section>
+        <section className="uservip-s4">
+          <div className="uservips4-nav">
+            <ul>
+              <li>
+                <a className="active">黃金</a>
+              </li>
+              <li>
+                <a>柏金</a>
+              </li>
+              <li>
+                <a>鑽石</a>
+              </li>
+            </ul>
+          </div>
+          <div className="uservips4-main">
+            <div className="s4maincontent">
+              <div className="uservips4-title">黃金會員</div>
+              <div className="uservips4-text">註冊會員即享有黃金會員之等級福利</div>
+              <div className="uservips4-card">
+                <div className="card-title">黃金會員專屬折扣</div>
+                <table className="card-table">
+                  <tbody>
+                    <tr>
+                      <th>升級條件 : </th>
+                      <td>首次註冊</td>
+                    </tr>
+                    <tr>
+                      <th>新會員禮 :</th>
+                      <td>購物金 $100</td>
+                    </tr>
+                    <tr>
+                      <th>當月壽星禮: </th>
+                      <td>贈購物金$50</td>
+                    </tr>
+                    <tr>
+                      <th>全館優惠 :</th>
+                      <td>
+                        單筆消費滿$5000
+                        <br />
+                        打95折
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <button className="card-button">前往註冊/登入</button>
+              </div>
+            </div>
+            <div className="s4mainimg">
+              <img src={vips4img} alt="" />
+            </div>
+          </div>
+        </section>
         <section className="uservip-s5">
           <div className="uservips5-contnet">
             <div className="page-type1-area-title">注意事項</div>

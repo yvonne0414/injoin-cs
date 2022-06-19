dd3917501b39a50d6a07ecb35263fc185ff4164d
Cheer <84440486+luap580101@users.noreply.github.com>
2022-06-20 00:25:10 +0800

    feat userInfo

--- injoin-react/src/components/UserInfo/UserInfoChangePasswd.js
+++ injoin-react/src/components/UserInfo/UserInfoChangePasswd.js
@@ -1,22 +1,34 @@
+import { Cascader, Input, Space } from 'antd';
+
 const UserInfoChangePasswd = () => {
+  const UserInfoChangePasswdArr = [
+    { cn: '原始密碼', en: 'originallypassword' },
+    { cn: '新密碼', en: 'newpassword' },
+    { cn: '確認新密碼', en: 'confirmpassword' },
+  ];
+
   return (
     <>
-      <form className="changepassword" action="">
-        <div className="changetable">
-          <div className="changepart">
-            <label htmlFor="originallypassword">原始密碼</label>
-            <input type="originallypassword" name="originallypassword" id="" />
-            <div className="changepart">
-              <label htmlFor="newpassword">新密碼</label>
-              <input type="newpassword" name="newpassword" id="" />
-            </div>
-          </div>
-          <div className="changepart">
-            <label htmlFor="confirmpassword">確認新密碼</label>
-            <input type="confirmpassword" name="confirmpassword" id="" />
-          </div>
+      <form className="userInfo-changepassword" action="" onSubmit={(e)=>{
+        e.preventDefault();
+      }}>
+        <div className="change-table">
+          <Space direction="vertical">
+            {UserInfoChangePasswdArr.map((v, i) => {
+              return (
+                <div className="change-part">
+                  <div className="change-name">{v.cn}</div>
+                  <Input.Password className="chenge-input" key={i} placeholder={v.cn} />
+                </div>
+              );
+            })}
+          </Space>
         </div>
-        <button className="injoin-btn-outline" onClick={(e)=>e.preventDefault}>更改密碼</button>
+        <div className="change-input">
+          <button className="injoin-btn-outline" onClick={(e) => e.preventDefault}>
+            更改密碼
+          </button>
+        </div>
       </form>
     </>
   );
--- injoin-react/src/components/UserInfo/UserProfile.js
+++ injoin-react/src/components/UserInfo/UserProfile.js
@@ -1,5 +1,16 @@
 import Button from 'react-bootstrap/Button';
 import iconURL from '../../assets/images/fe/membercenter/usericon.png';
+const userInfoMemberarr= [
+  {cn:"真實姓名", en:"memberfullname"},
+  {cn:"線上暱稱", en:"membername"},
+  {cn:"電子郵件", en:"memberemail"},
+  {cn:"出生日期", en:"memberbirth"},
+  {cn:"手機號碼", en:"memberphone"},
+  {cn:"居家住址", en:"memberhome"},
+  {cn:"真實姓名", en:"memberfullname"},
+]
+
+
 
 const UserProfile = () => {
   return (
@@ -21,7 +32,7 @@ const UserProfile = () => {
       </div>
       <div className="input-item col-12 my-2 my-md-4">
         <label htmlFor="username" className="w-25 text-end">
-          線上暱稱：
+          ：
         </label>
         <input type="text" name="username" className="w-50 user-input" />
       </div>

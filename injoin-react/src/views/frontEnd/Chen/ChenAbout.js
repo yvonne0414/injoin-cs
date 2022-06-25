fe2267729fbe01b247c35a723972f5fa7a650e36
Cheer <luap580101@gmail.com>
2022-06-25 14:54:42 +0800

    feat logout

---
+++ injoin-react/src/views/frontEnd/Chen/ChenAbout.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { API_URL, IMAGE_URL } from '../../../utils/config';
+
+const ChenAbout = () => {
+  const [member, setMember] = useState(null);
+  useEffect(() => {
+    let getMemberInfo = async () => {
+      let response = await axios.get(`${API_URL}/member/info`, {
+        // 允許跨源讀寫 cookie
+        // 這樣才可以把之前有紀錄登入資料的 session id 送回去後端
+        withCredentials: true,
+      });
+      console.log(response.data);
+      setMember(response.data);
+    };
+    getMemberInfo();
+  }, []);
+
+  // member.photo /members/1655003608497.jpg
+  return (
+    <div className="m-7">
+      <h2 className="m-7 text-2xl text-gray-600">這是登入</h2>
+      {member ? (
+        <>
+          <h3>Hi, {member.name}</h3>
+          <h2>useriD : {member.id}</h2>
+          
+        </>
+      ) : (
+        <h3>尚未登入</h3>
+      )}
+    </div>
+  );
+};
+
+export default ChenAbout;
